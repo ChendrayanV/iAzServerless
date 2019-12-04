6f8629526941578e7ff45b73e7d03cfe5b3998fc
C.Venkatesan <C.Venkatesan@Shell.com>
2019-12-04 19:16:04 +0530

    added ezra potluck endpoint API

--- EzraPotLuckMenu/run.ps1
+++ EzraPotLuckMenu/run.ps1
@@ -1,28 +1,50 @@
 using namespace System.Net
-
-# Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
-
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
-
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$body = html -Content {
+    head -Content {
+        Title -Content "Contoso EzraPotLuckMenuItem"
+        Link -href 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' -rel stylesheet
+        script -src 'https://code.jquery.com/jquery-3.2.1.slim.min.js'
+        script -src 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
+        script -src 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+    }
+    Body -Content {
+        Div -Class 'container' -Content {
+            Div -Class 'jumbotron' -Content {
+                H1 -Class 'display-4' -Content "Ezra Potluck Menu"
+            }
+        }
+        
+        Div -Class 'container' -Content {
+            $EzraPotLuckMenuItems = Get-Content .\database\EzraPotLuckMenu.json | ConvertFrom-Json
+            Table -class 'table' -Content {
+                Thead -Content {
+                    Th -Content "ID"
+                    Th -Content "First Name"
+                    Th -Content "Sur Name"
+                    Th -Content "Date"
+                    Th -Content "Category"
+                    Th -Content "Dish"
+                }
+                Tbody -Content {
+                    foreach ($EzraPotLuckMenuItem in $EzraPotLuckMenuItems) {
+                        tr -Content {
+                            td -Content $EzraPotLuckMenuItem.ID
+                            td -Content $EzraPotLuckMenuItem.FirstName
+                            td -Content $EzraPotLuckMenuItem.SurName
+                            td -Content $EzraPotLuckMenuItem.Date
+                            td -Content $EzraPotLuckMenuItem.Category
+                            td -Content $EzraPotLuckMenuItem.Dish
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
-if ($name) {
-    $status = [HttpStatusCode]::OK
-    $body = "Hello $name"
-}
-else {
-    $status = [HttpStatusCode]::BadRequest
-    $body = "Please pass a name on the query string or in the request body."
-}
-
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = $status
-    Body = $body
-})
+        headers    = @{'content-type' = 'text/html' }
+        StatusCode = [httpstatuscode]::OK
+        Body       = $body
+    })
